2af345fc6ceff6bdf71542a7b5b769f46c8c9066
droquee3 <caiors3347@gmail.com>
2024-09-28 18:04:37 -0300

    v2.2.6

--- Projeto/Assets/Programação/Scripts/AlinharCamera.cs
+++ Projeto/Assets/Programação/Scripts/AlinharCamera.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class AlinharCamera : MonoBehaviour
 {
-    public Transform cameraTransform;  // Referência à câmera
-    public float rotationSpeed = 5f;   // Velocidade de rotação do personagem
+    public Transform cameraTransform; 
+    public float rotationSpeed = 5f;  
     private Rigidbody rb;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();  // Obtém o Rigidbody do personagem
-        rb.freezeRotation = true;        // Travar a rotação do Rigidbody
+        rb = GetComponent<Rigidbody>();  
+        rb.freezeRotation = true;        
     }
 
     void Update()
     {
-        // Calcular a direção em que a câmera está olhando, ignorando a componente Y
+       
         Vector3 directionToCamera = cameraTransform.forward;
-        directionToCamera.y = 0f;  // Zera a componente Y para manter o personagem "no chão"
+        directionToCamera.y = 0f; 
 
-        // Se a direção for maior que um valor muito pequeno, aplicar a rotação
+     
         if (directionToCamera.sqrMagnitude > 0.01f)
         {
             Quaternion targetRotation = Quaternion.LookRotation(directionToCamera);
