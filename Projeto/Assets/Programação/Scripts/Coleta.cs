5dc9d4ed2d31f73f165c896df37b0a194010f975
droquee3 <caiors3347@gmail.com>
2024-11-09 20:21:56 -0300

    v2.5.4

--- Projeto/Assets/Programação/Scripts/Coleta.cs
+++ Projeto/Assets/Programação/Scripts/Coleta.cs
@@ -4,25 +4,12 @@ using UnityEngine;
 
 public class Coleta : MonoBehaviour
 {
-    public GameObject esferaPrincipal;  
-    public int totalEsferas = 3;        
-    private int esferasColetadas = 0;   
-
+  
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("EsferaColetavel"))
         {
             other.gameObject.SetActive(false);
-
-            esferasColetadas++;
-
-            if (esferasColetadas >= totalEsferas)
-            {
-                if (esferaPrincipal != null)
-                {
-                    esferaPrincipal.SetActive(true);
-                }
-            }
         }
     }
 }
